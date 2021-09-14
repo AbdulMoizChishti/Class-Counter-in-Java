03c7005d806636f95b08479ca0c94998f7e9f753
AbdulMoizChishti <moaiz599@gmail.com>
2021-09-14 18:03:38 +0500

    Class Counter

---
+++ src/classcounter/A.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classcounter;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class A {
+    
+   public static int count=0;
+    
+   public static void counter(){
+   count++;
+       System.out.println("Counter: "+count);
+   }
+    
+    
+}
---
+++ src/classcounter/Classcounter.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classcounter;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class Classcounter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A obj =new A();
+        obj.counter();
+        obj.counter();
+         obj.counter();
+        obj.counter();
+        
+        
+        A obj1 =new A();
+        obj1.counter();
+    }
+    
+}
